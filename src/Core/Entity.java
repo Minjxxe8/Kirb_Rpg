89ade2e11f2756e3209d3d4ba5fa5b10316cb8e7
Robl Elisabeth <elisabeth.robl@ynov.com>
2026-02-16 09:19:35 +0100

    feature(entity): class entity

--- src/Core/Entity.java
+++ src/Core/Entity.java
@@ -1,5 +1,28 @@
 package Core;
 
-public class Entity {
+abstract class Entity {
+    protected String name;
+    protected int hp;
+    protected int maxHp;
 
+    public Entity(String name, int maxHp) {
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    public void takeDamage(int damage) {
+        this.hp = Math.max(0, this.hp - damage);
+        System.out.println(name + " reçoit " + damage + " dégâts !");
+    }
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void doAction();
 }
