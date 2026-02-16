59fbe7f5d08259cfbb7d22e8b3c348aff9f4e92c
Robl Elisabeth <elisabeth.robl@ynov.com>
2026-02-16 15:44:07 +0100

    feature(entity): class Kirby

--- src/Core/Kirby.java
+++ src/Core/Kirby.java
@@ -1,39 +1,56 @@
 package Core;
+
 import Powers.Power;
 
-class Kirby {
-    private Power state;
-    private final int MAX_HP = 100;
-    private int hp = MAX_HP;
-    private static int enemiesDefeated = 0;
+class Kirby extends Entity {
+    private static final int MAX_HP = 100;
+    private final Inventory inventory;
+    private Power currentPower;
 
-    public static int getEnemiesDefeated() {
-        return enemiesDefeated;
+    public Kirby() {
+        super("Kirby", MAX_HP);
+        this.inventory = new Inventory(0, 0);
     }
 
-    public static void setEnemiesDefeated(int enemiesDefeated) {
-        Kirby.enemiesDefeated = enemiesDefeated;
+    public void heal() {
+        if (inventory.usePotion()) {
+            this.hp = Math.min(maxHp, hp + 40);
+            System.out.println("Kirby utilise une potion. PV: " + hp + ". Potions restantes: " + inventory.getPotionCount());
+        } else {
+            System.out.println("Plus de potions !");
+        }
     }
 
-    public void takeDamage(int damage) {
-        this.hp = Math.max(0, this.hp - damage);
-        System.out.println("Kirby a reçu " + damage + " dégâts !");
-        displayHealthBar();
+    public void addGold(int amount) {
+        inventory.addGold(amount);
+    }
+
+    public void addPotions(int amount) {
+        inventory.addPotions(amount);
+    }
+
+    public void showInventory() {
+        System.out.println("Inventaire -> Potions: " + inventory.getPotionCount() + " | Or: " + inventory.getGold());
     }
 
     public void displayHealthBar() {
         int barLength = 20;
-        double healthRatio = (double) hp / MAX_HP;
+        double healthRatio = (double) hp / maxHp;
         int filledMarkers = (int) (healthRatio * barLength);
 
         StringBuilder bar = new StringBuilder("[");
         for (int i = 0; i < barLength; i++) {
-            if (i < filledMarkers) bar.append("=");
-            else bar.append(" ");
+            if (i < filledMarkers) {
+                bar.append("=");
+            } else {
+                bar.append(" ");
+            }
         }
-        bar.append("] " + hp + "/" + MAX_HP + " PV");
+        bar.append("] " + hp + "/" + maxHp + " PV");
 
-        System.out.println(bar.toString());
-        if (hp <= 0) System.out.println("GAME OVER : Kirby est K.O. !");
+        System.out.println(bar);
+        if (hp <= 0) {
+            System.out.println("GAME OVER : Kirby est K.O. !");
+        }
     }
 }
